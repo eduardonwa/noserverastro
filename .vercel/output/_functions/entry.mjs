cf8948c720211e2df1831ce3d9f98f69ec231046
eduardonwa <eduardongua@hotmail.com>
2025-02-22 09:27:20 -0700

    ran the build command

--- .vercel/output/_functions/entry.mjs
+++ .vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports } from './chunks/entrypoint_DMXJ5sIP.mjs';
-import { manifest } from './manifest_3JUL3fiY.mjs';
+import { manifest } from './manifest_CWeuh-iN.mjs';
 
 const serverIslandMap = new Map();;
 
@@ -20,7 +20,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "2bb2bcb2-1948-4c53-8061-6bf60a00d820",
+    "middlewareSecret": "7a73874c-4280-44c5-89fd-90c36890c891",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
